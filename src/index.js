3b4dd78f1aed8db54b0d9f720231bae221663dd8
Rahul Madaan <44020002+rahulmadaan@users.noreply.github.com>
2019-04-02 14:51:07 +0530

    Initial commit.. add working application
    Calculator in react js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// import css from "../public/style.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { Hour: 0, minute: 0, second: 0, miliSecond: 0 };
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  tick() {
+    this.setState(state => ({
+      miliSecond: state.miliSecond + 1
+    }));
+  }
+
+  incrementSecond() {
+    this.setState(state => ({
+      second: state.second + 1
+    }));
+    if (this.state.second > 60) {
+      this.setState(state => ({ second: 0 }));
+      this.incrementMinute();
+    }
+  }
+
+  updateHour() {
+    this.setState(state => ({
+      hour: state.hour + 1
+    }));
+  }
+
+  incrementMinute() {
+    this.setState(state => ({
+      minute: state.minute + 1
+    }));
+    if (this.state.minute > 60) {
+      this.updateHour();
+    }
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => this.incrementSecond(), 1000);
+  }
+
+  clearInterval() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        Hour : {this.state.Hour}
+        Minute : {this.state.minute}
+        Seconds : {this.state.second}
+        Milisecond : {this.state.miliSecond}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Timer />, document.getElementById("root"));
